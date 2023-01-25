0627b8fdede55a5b5342a68af3a3ece1d2dcc5c8
tainaragent007 <tainaragent007@gmail.com>
2023-01-25 17:07:03 -0300

    add login.py

---
+++ login.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request, jsonify
+
+
+login = Blueprint('login', __name__)
+
+
+@login.route('/login', methods=['POST'])
+def llamarServicioSet():
+    global user,passw
+    ##try:
+    user =request.json['user']
+    passw =request.json['passw']
+    print("Username",user)
+    print("Password",passw)
+
+    #inicializarVariables(categoryId)
