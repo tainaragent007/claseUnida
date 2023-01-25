f7bbf4749a07d25f99e5532abe34670e564de77d
tainaragent <114042449+tainaragent007@users.noreply.github.com>
2023-01-25 15:51:34 -0300

    Add files via upload

---
+++ from flask import Flask.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+#servivcios rest
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hola Mundo''
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug = True, port = 5000)
+    app.run(debug = True)
