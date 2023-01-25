d8e71b70a56501c0340f90c0dca850657e3cd4b2
tainaragent007 <tainaragent007@gmail.com>
2023-01-25 17:30:13 -0300

    aun no se que pedos

--- login.py
+++ login.py
@@ -1,15 +1,12 @@
 from flask import Blueprint, request, jsonify
-from login import login 
-
 login = Blueprint('login', __name__)
-
-
 @login.route('/login', methods=['POST'])
 def llamarServicioSet():
     global user,passw
     ##try:
     user =request.json['user']
     passw =request.json['passw']
+    
     print("Username",user)
     print("Password",passw)
 
--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def hello():
     return "Hola Mundo"
 
 if __name__ == "__main__":
-    
+
     app.run(host = '0.0.0.0', debug = True, port = 5001)
     app.run(debug = True)
