f4d322408fe1b5244c29007985e02941dd675127
tainaragent007 <tainaragent007@gmail.com>
2023-01-25 16:52:44 -0300

    add mas archivos

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+#! /usr/bin/env python3
 from flask import Flask
-
-
 app = Flask(__name__)
 
-#servivcios rest
-
-@app.route('/', methods=['GET'])
+##servicios rest
+@app.route("/", methods=['GET'])
 def hello():
-    return 'Hola Mundo'
+    return "Hola Mundo"
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug = True, port = 5000)
+    app.run(host = '0.0.0.0', debug = True, port = 5001)
     app.run(debug = True)
