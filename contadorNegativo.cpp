2656499aeb5f79292545c1977dbd7542994c0f57
tainaragent007 <tainaragent007@gmail.com>
2023-01-27 17:07:41 -0300

    contadores positivos y negativos

---
+++ contadorNegativo.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "stdlib.h"
+
+using namespace std; 
+int main()
+{
+    for(int i=10; i>0; i--)
+    {//Notemos que escribir i-- es similar a escribir i = i - 1
+        cout << i << endl;
+    }
+    system("PAUSE");
+    return 0;
+}
