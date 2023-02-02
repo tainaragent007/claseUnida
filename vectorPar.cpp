d1a31d71eb087dab7353212ec58055fe5e9d8d62
tainaragent <tainaragent007@gmail.com>
2023-02-02 15:31:52 -0300

    vectorPar

--- vectorPar.cpp
+++ vectorPar.cpp
@@ -1,23 +1,31 @@
-#iostream
-{
-    cont=0;
-    if(x%2==0)
+#include "iostream"
+
+
+{   int x,cont,x,i,vectorP[100];
+    int j=0, vectorI[100];
+    i=0;
+    for(x=1;x<=100;x++)
     {
-        tabla[i]=x;
-        i++;
-    }else{
-        vectorP[j]=x;
-        j++;
+        cont=0;
+        if(x%2==0)
+        {
+            vectorP[i]=x;
+            i++;
+        }else{
+            vectorP[j]=x;
+            j++;
+        }
     }
+    cout<<"Vector Par:"<<endl;
+    for (x=0;x<i;x++)
+    {
+        cout<<'Par:'<<vectorP[x]<<endl;
+    }
+    cout<<"Vector Impar:"<<endl;
+    for (x=0;x<j;x++)
+    {
+        cout<<"Impar:"<<vectorI[x]<<endl;
+    }
+    system("PAUSE");
+    return 0;
 }
-cout<<"Vector Par: "<<endl;
-for (x=0;x<i;x++)
-{
-    cout<<tabla[x]<<endl;
-}
-for (x=0;j<i;x++)
-{
-    cout<<vectorP[x]<<endl;
-}
-system("PAUSE");
-return 0;
